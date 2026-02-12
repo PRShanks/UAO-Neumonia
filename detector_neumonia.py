02765a79013a5ea963e730c1fbd7fbf570202f78
marco antonio chaparro morales <marco.chaparro@uao.edu.co>
2026-02-12 17:23:57 -0500

    modificar modulos

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -25,50 +25,25 @@ import time
 
 import tensorflow as tf
 K = tf.keras.backend
-import pydicom as dicom 
-tf.compat.v1.disable_eager_execution()
-tf.compat.v1.experimental.output_all_intermediates(True)
+import pydicom as dicom
 import cv2
 import os
 
+from load_model import load_trained_model
+
+
 def model_fun():
-    
     if not hasattr(model_fun, "model"):
         base_dir = os.path.dirname(os.path.abspath(__file__))
         model_path = os.path.join(base_dir, "models", "conv_MLP_84.h5")
-        model_fun.model = tf.keras.models.load_model(model_path)
+        model_fun.model = load_trained_model(model_path)
     return model_fun.model
 
 
 
 
-def grad_cam(array):
-    img = preprocess(array)
-    model = model_fun()
-    preds = model.predict(img)
-    argmax = np.argmax(preds[0])
-    output = model.output[:, argmax]
-    last_conv_layer = model.get_layer("conv10_thisone")
-    grads = K.gradients(output, last_conv_layer.output)[0]
-    pooled_grads = K.mean(grads, axis=(0, 1, 2))
-    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
-    pooled_grads_value, conv_layer_output_value = iterate(img)
-    for filters in range(64):
-        conv_layer_output_value[:, :, filters] *= pooled_grads_value[filters]
-    # creating the heatmap
-    heatmap = np.mean(conv_layer_output_value, axis=-1)
-    heatmap = np.maximum(heatmap, 0)  # ReLU
-    heatmap /= np.max(heatmap)  # normalize
-    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[2]))
-    heatmap = np.uint8(255 * heatmap)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    img2 = cv2.resize(array, (512, 512))
-    hif = 0.8
-    transparency = heatmap * hif
-    transparency = transparency.astype(np.uint8)
-    superimposed_img = cv2.add(transparency, img2)
-    superimposed_img = superimposed_img.astype(np.uint8)
-    return superimposed_img[:, :, ::-1]
+from preprocess_img import preprocess
+from grad_cam import grad_cam
 
 
 def predict(array):
@@ -76,7 +51,6 @@ def predict(array):
     batch_array_img = preprocess(array)
     #   2. call function to load model and predict: it returns predicted class and probability
     model = model_fun()
-    # model_cnn = tf.keras.models.load_model('conv_MLP_84.h5')
     prediction = np.argmax(model.predict(batch_array_img))
     proba = np.max(model.predict(batch_array_img)) * 100
     label = ""
@@ -87,40 +61,18 @@ def predict(array):
     if prediction == 2:
         label = "viral"
     #   3. call function to generate Grad-CAM: it returns an image with a superimposed heatmap
-    heatmap = grad_cam(array)
+    heatmap = grad_cam(array, model)
     return (label, proba, heatmap)
 
 
-def read_dicom_file(path):
-    img = dicom.dcmread(path)
-    img_array = img.pixel_array
-    img2show = Image.fromarray(img_array)
-    img2 = img_array.astype(float)
-    img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
-    img2 = np.uint8(img2)
-    img_RGB = cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)
-    return img_RGB, img2show
-
-
-def read_jpg_file(path):
-    img = cv2.imread(path)
-    img_array = np.asarray(img)
-    img2show = Image.fromarray(img_array)
-    img2 = img_array.astype(float)
-    img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
-    img2 = np.uint8(img2)
-    return img2, img2show
+from read_img import read_jpg_file, read_dicom_file
 
 
 def preprocess(array):
-    array = cv2.resize(array, (512, 512))
-    array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-    array = clahe.apply(array)
-    array = array / 255
-    array = np.expand_dims(array, axis=-1)
-    array = np.expand_dims(array, axis=0)
-    return array
+    # kept for backward compatibility; delegate to `preprocess_img.preprocess`
+    from preprocess_img import preprocess as _preprocess
+
+    return _preprocess(array)
 
 
 class App:
---
+++ grad_cam.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+
+def grad_cam(array, model, last_conv_layer_name: str = "conv10_thisone"):
+    # prepare image batch
+    from preprocess_img import preprocess
+
+    img = preprocess(array)
+
+    # build a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    last_conv_layer = model.get_layer(last_conv_layer_name)
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [last_conv_layer.output, model.output]
+    )
+
+    # compute the gradient of the top predicted class for our input image
+    preds = model.predict(img)
+    pred_index = np.argmax(preds[0])
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img)
+        loss = predictions[:, pred_index]
+
+    grads = tape.gradient(loss, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2)).numpy()
+
+    conv_outputs = conv_outputs.numpy()[0]
+    for i in range(conv_outputs.shape[-1]):
+        conv_outputs[:, :, i] *= pooled_grads[i]
+
+    heatmap = np.mean(conv_outputs, axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    if np.max(heatmap) != 0:
+        heatmap /= np.max(heatmap)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[2]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+    img2 = cv2.resize(array, (512, 512))
+    hif = 0.8
+    transparency = heatmap * hif
+    transparency = transparency.astype(np.uint8)
+    superimposed_img = cv2.add(transparency, img2)
+    superimposed_img = superimposed_img.astype(np.uint8)
+    return superimposed_img[:, :, ::-1]
